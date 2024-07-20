64017590650f4761dd7fe1838199090c9b5e5c1f
Moha-yusuf <Mohaa.yusuf11@gmail.com>
2024-07-20 16:46:52 -0500

    Setup AplicationDbContext

---
+++ TayoWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TayoWeb.Models;
+
+namespace TayoWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
