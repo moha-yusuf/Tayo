60b644608a2c3d207d3c3ef42021146b21e15add
Moha-yusuf <Mohaa.yusuf11@gmail.com>
2024-08-06 19:45:02 -0500

    Get all categories from Category table

--- TayoWeb/Controllers/CategoryController.cs
+++ TayoWeb/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TayoWeb.Data;
+using TayoWeb.Models;
 
 namespace TayoWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
