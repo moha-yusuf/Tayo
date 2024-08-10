d074b55d877218056243fe1c55d393b3dd1fe527
Moha-yusuf <Mohaa.yusuf11@gmail.com>
2024-08-10 15:43:33 -0500

    Add Create action and view

--- TayoWeb/Controllers/CategoryController.cs
+++ TayoWeb/Controllers/CategoryController.cs
@@ -17,5 +17,10 @@ namespace TayoWeb.Controllers
             List<Category> categoryList = _db.Categories.ToList();
             return View(categoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
