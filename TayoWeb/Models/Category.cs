594accea6f1447e5b876864a2f521aa2b8ba1027
Moha-yusuf <Mohaa.yusuf11@gmail.com>
2024-08-17 17:07:57 -0500

    Design Create Category UI

--- TayoWeb/Models/Category.cs
+++ TayoWeb/Models/Category.cs
@@ -9,6 +9,7 @@ namespace TayoWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
